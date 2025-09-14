9e3756df9aaed71c157b51247b0b647bd76e8b03
Joel Savitz <joel@beamshyft.com>
2025-09-14 16:45:58 -0400

    fix legal name in footer
    Signed-off-by: Joel Savitz <joel@beamshyft.com>

--- src/components/beamshyft-footer.tsx
+++ src/components/beamshyft-footer.tsx
@@ -92,7 +92,7 @@ export const BeamshyftFooter = () => (
       </FooterColumn>
     </FooterContent>
     <FooterBottom>
-      <div>© 2025 Beamshyft. All rights reserved</div>
+      <div>© 2025 Beamshyft LLC. All rights reserved</div>
       <div className="flex items-center gap-4">
       </div>
     </FooterBottom>
