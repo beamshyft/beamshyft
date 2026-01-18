ccbd4ba5d95629b9dc1a4bdbd036b0bc6dd11801
Joel Savitz <joel@beamshyft.com>
2026-01-18 13:06:31 -0500

    update logo
    Signed-off-by: Joel Savitz <joel@beamshyft.com>

--- src/components/logos/beamshyft.tsx
+++ src/components/logos/beamshyft.tsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const BeamshyftLogo: React.FC<React.ImgHTMLAttributes<HTMLImageElement>> = (props) => {
     return (<img
-        src="/logo.jpg"
+        src="/logo.png"
         alt="Beamshyft Logo"
         width={props.width || 164}
         className="block pt-1"
